d27816a8b97cff55bc042fe6ccf7486b1d6ce731
osidekyle <hawkinskyle67@gmail.com>
2021-09-10 23:24:22 -0700

    fin for today

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,42 @@
 import org.apache.avro.*;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args){
+
+    public void doThing(){
+        Schema.Parser parser = new Schema.Parser();
+        try {
+            Schema schema = parser.parse(
+                    getClass().getResourceAsStream("StringPair.avsc")
+            );
+            GenericRecord datum = new GenericData.Record(schema);
+            datum.put("left","L");
+            datum.put("right","R");
+            ByteArrayOutputStream out=new ByteArrayOutputStream();
+            DatumWriter<GenericRecord> writer= new GenericDatumWriter<GenericRecord>(schema);
+            Encoder encoder= EncoderFactory.get().binaryEncoder(out,null);
+            writer.write(datum,encoder);
+            encoder.flush();
+            out.close();
+
+            DatumReader<GenericRecord> reader = new GenericDatumReader<GenericRecord>(schema);
+            Decoder decoder = DecoderFactory.get().binaryDecoder(out.toByteArray(),null);
+            GenericRecord result = reader.read(null,decoder);
+
+        }
+        catch (IOException ex){
+            System.out.println("darn");
+        }
 
     }
 }
+
+
---
+++ src/main/java/yuh.java
@@ -0,0 +1,6 @@
+public class yuh{
+    public static void main(String[] args){
+        Main main=new Main();
+        main.doThing();
+    }
+}
